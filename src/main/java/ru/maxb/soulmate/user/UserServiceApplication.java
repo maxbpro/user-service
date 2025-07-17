99f5fbb98c63104a6463c333ea485086a87ff410
mbuyanov <mbuyanov@roox.ru>
2025-07-17 19:56:47 +0300

    Hello world commit

---
+++ src/main/java/ru/maxb/soulmate/user/UserServiceApplication.java
@@ -0,0 +1,17 @@
+package ru.maxb.soulmate.user;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserServiceApplication.class, args);
+	}
+
+	@RequestMapping("/")
+	public String home() {
+		return "Hello World";
+	}
+}
