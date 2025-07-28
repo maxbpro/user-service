a11febd5ad1331895411209dda8b9784aa07ab11
mbuyanov <mbuyanov@roox.ru>
2025-07-28 19:13:33 +0300

    Added test controller

--- src/main/java/ru/maxb/soulmate/user/UserController.java
+++ src/main/java/ru/maxb/soulmate/user/UserController.java
@@ -2,13 +2,15 @@ package ru.maxb.soulmate.user;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RequiredArgsConstructor
-@RestController("/api/users/v1")
+@RestController
+@RequestMapping("/api/users/v1")
 public class UserController {
 
-    @GetMapping("/")
+    @GetMapping("")
     public String test() {
         return "Hello World";
     }
