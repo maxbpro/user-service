b3ea3283b9dd5ee37985981414f411526cda3359
mbuyanov <mbuyanov@roox.ru>
2025-09-18 17:27:53 +0300

    Added loki-appender

--- src/main/java/ru/maxb/soulmate/user/UserController.java
+++ src/main/java/ru/maxb/soulmate/user/UserController.java
@@ -1,6 +1,8 @@
 package ru.maxb.soulmate.user;
 
 import lombok.RequiredArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,8 +12,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/users/v1")
 public class UserController {
 
+    private static final Logger log = LoggerFactory.getLogger(UserController.class);
+
+
     @GetMapping("")
     public String test() {
+        log.info("Test Hello World");
         return "Hello World";
     }
 }
